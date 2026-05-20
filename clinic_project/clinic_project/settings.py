b26ad7ff800b9ea17f3e4ed21ac039c9bfd9e08f
Anuja khatri <khatrianuja20@gmail.com>
2026-05-20 09:25:38 +0545

    chore:maintain migrations files into postgres database

--- clinic_project/clinic_project/settings.py
+++ clinic_project/clinic_project/settings.py
@@ -76,7 +76,7 @@ ROOT_URLCONF = 'clinic_project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / "templates"],
+        'DIRS': [],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -95,11 +95,16 @@ CSRF_TRUSTED_ORIGINS = ["http://127.0.0.1:8000"]
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+   'default': {
+       'ENGINE': 'django.db.backends.postgresql',
+       'NAME': 'clinic',
+       'USER': 'postgres',
+       'PASSWORD': '',
+       'HOST': 'localhost',
+       'PORT': '5432',
+   }
 }
+
 
 
 # Password validation
