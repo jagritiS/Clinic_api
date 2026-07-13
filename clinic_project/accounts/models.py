fe665777be472bae8b43bd9a0c6493b753119bf4
Anuja khatri <khatrianuja20@gmail.com>
2026-07-13 07:34:33 +0545

    refactor: comment out save method in User model for future implementation

--- clinic_project/accounts/models.py
+++ clinic_project/accounts/models.py
@@ -19,13 +19,13 @@ class User(AbstractUser):
             return 'admin'
         return self.role
 
-    def save(self, *args, **kwargs):
-        # Hash the password if it's not already hashed
-        if self.password and not (self.password.startswith('pbkdf2_sha256$') or 
-                                 self.password.startswith('bcrypt$') or 
-                                 self.password.startswith('argon2$')):
-            self.set_password(self.password)
-        super().save(*args, **kwargs)
+    # def save(self, *args, **kwargs):
+    #     # Hash the password if it's not already hashed
+    #     if self.password and not (self.password.startswith('pbkdf2_sha256$') or 
+    #                              self.password.startswith('bcrypt$') or 
+    #                              self.password.startswith('argon2$')):
+    #         self.set_password(self.password)
+    #     super().save(*args, **kwargs)
 
     def __str__(self):
         return self.username
